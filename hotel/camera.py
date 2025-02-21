9340529bf74fd58a40ed96692385970d01b94e4d
Francesca <fraquilani@itsmeccanicabruzzo.eu>
2025-02-21 19:24:43 +0100

    feat: camera/prenotazione

--- hotel/camera.py
+++ hotel/camera.py
@@ -2,3 +2,6 @@ class Camera:
     def __init__ (self, numero_camera, piano):
         self.numero_camera = numero_camera
         self.piano = piano
+
+    def __str__ (self):
+        return f"Numero camera: {self.numero_camera}, piano: {self.piano}"
--- hotel/prenotazione.py
+++ hotel/prenotazione.py
@@ -3,4 +3,7 @@ class Prenotazione:
         self.codice = codice
         self.data_check_in = data_check_in
         self.data_check_out = data_check_out
+    
+    def __str__ (self):
+        return f"Codice: {self.codice}, data_check_in: {self.data_check_in}, data_check_out: {self.data_check_out}"
         
