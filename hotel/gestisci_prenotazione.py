1c46e0b208875ef5f73d6f2d6a918e064c0b69f8
Francesca <fraquilani@itsmeccanicabruzzo.eu>
2025-02-21 18:58:11 +0100

    feat: gestisci_prenotazione

---
+++ hotel/gestisci_prenotazione.py
@@ -0,0 +1,6 @@
+
+
+
+def registra_nuove_prenotazioni(nome, numero_camera, data_check_in, data_check_out):
+    nuova_prenotazione = {"nome": nome, "numero_camera" : numero_camera, "data_check_in" : data_check_in, "data_check_out" : data_check_out}
+    prenotazione.append(nuova_prenotazione)
---
+++ hotel/prenotazione.py
@@ -0,0 +1,6 @@
+class Prenotazione: 
+    def __init__ (self, codice, data_check_in, data_check_out):
+        self.codice = codice
+        self.data_check_in = data_check_in
+        self.data_check_out = data_check_out
+        
