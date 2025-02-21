9dfb7d60390e1205961e36b23be2e9c1972bc586
Francesca <fraquilani@itsmeccanicabruzzo.eu>
2025-02-21 19:18:05 +0100

    feat: gestisci_prenotazione

---
+++ hotel/camera.py
@@ -0,0 +1,4 @@
+class Camera: 
+    def __init__ (self, numero_camera, piano):
+        self.numero_camera = numero_camera
+        self.piano = piano
--- hotel/gestisci_prenotazione.py
+++ hotel/gestisci_prenotazione.py
@@ -1,6 +1,12 @@
+import datetime
 
-
+Camera = []
+Cliente = []
+Prenotazione = []
 
 def registra_nuove_prenotazioni(nome, numero_camera, data_check_in, data_check_out):
     nuova_prenotazione = {"nome": nome, "numero_camera" : numero_camera, "data_check_in" : data_check_in, "data_check_out" : data_check_out}
-    prenotazione.append(nuova_prenotazione)
+    Prenotazione.append(nuova_prenotazione)
+
+registra_nuove_prenotazioni("Carlo", 32, datetime.date(2025, 1, 20), datetime.date(2025, 1, 30))
+registra_nuove_prenotazioni("Franco", 19, datetime.date(2025, 7, 15), datetime.date(2025, 7, 20))
