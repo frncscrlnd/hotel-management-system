d1978d66a1507e3571299ee23023e921aeecb47a
Francesca <fraquilani@itsmeccanicabruzzo.eu>
2025-02-21 18:59:00 +0100

    feat: cliente

--- hotel/cliente.py
+++ hotel/cliente.py
@@ -1,5 +1,9 @@
 class Cliente: 
-    def __init__ (self, nome, cognome, cod_fis):
-        self.nome = nome
-        self.cognome = cognome
+    def __init__ (self, nome_cliente, cognome_cliente, cod_fis):
+        self.nome_cliente = nome_cliente
+        self.cognome_cliente = cognome_cliente
         self.cod_fis = cod_fis
+    
+    def __str__ (self):
+        return f"Nome cliente: {self.nome_cliente}, cognome cliente: {self.cognome_cliente}, codice fiscale del cliente: {self.cod_fis}"
+    
