96b5e37d9db4fa9544775f96dc91b07daaec9da6
fra <francescorla04@gmail.com>
2025-02-24 14:33:16 +0100

    Delete camera.py

--- hotel/camera.py
+++
@@ -1,7 +0,0 @@
-class Camera: 
-    def __init__ (self, numero_camera, piano):
-        self.numero_camera = numero_camera
-        self.piano = piano
-
-    def __str__ (self):
-        return f"Numero camera: {self.numero_camera}, piano: {self.piano}"
