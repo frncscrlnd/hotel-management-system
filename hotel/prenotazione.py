603663e5b953f3a29264e7a5c5ad0709ece980f9
fra <francescorla04@gmail.com>
2025-02-24 14:33:35 +0100

    Delete prenotazione.py

--- hotel/prenotazione.py
+++
@@ -1,9 +0,0 @@
-class Prenotazione: 
-    def __init__ (self, codice, data_check_in, data_check_out):
-        self.codice = codice
-        self.data_check_in = data_check_in
-        self.data_check_out = data_check_out
-    
-    def __str__ (self):
-        return f"Codice: {self.codice}, data_check_in: {self.data_check_in}, data_check_out: {self.data_check_out}"
-        
